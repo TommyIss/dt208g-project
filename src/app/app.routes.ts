7b9a0bf211ad82db202d7edf2568cc7eb184118a
Tommy Issa <tommyissa@hotmail.com>
2025-07-14 01:09:55 +0200

    Har skapat komponenter för sidorna

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MyScheduleComponent } from './pages/my-schedule/my-schedule.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'mySchedule', component: MyScheduleComponent},
+    {path: '', redirectTo: 'home', pathMatch: 'full'}
+];
---
+++ src/app/pages/my-schedule/my-schedule.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-schedule',
+  imports: [],
+  templateUrl: './my-schedule.component.html',
+  styleUrl: './my-schedule.component.scss'
+})
+export class MyScheduleComponent {
+
+}
